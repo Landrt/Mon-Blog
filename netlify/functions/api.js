a80fe03687bda5eb07e8f4b1864b8b4dd7796264
The Artist <artist@blog.com>
2026-03-23 00:45:32 +0100

    Fix Netlify serverless routing basePath

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,4 +2,6 @@ const serverless = require('serverless-http');
 const app = require('../../src/app');
 
 // Wrap the Express app for AWS Lambda (used by Netlify Functions)
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app, {
+  basePath: '/.netlify/functions/api'
+});
